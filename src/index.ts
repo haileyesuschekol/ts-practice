9d57efdb27d688c7f313b4c79e3ac3a3a97ffeae
Haileyesus Chekol <teamdevs7@gmail.com>
2024-04-30 11:52:52 +0100

    built in types in typescript

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+// types
+let sales: number = 1234 //number
+let course: string = "TypeScript" //string
+let is_published: boolean = true //boolean
+let level //any
+let array: number[] = [1, 2, 3] //number
+
+function fName(doc: string) {
+  console.log(doc)
+}
+
+// tuple
+// use it when we have two value pair
+const user: [number, string] = [1, "userName"]
+
+//enums
+// declare a constants
+const enum Size {
+  Small = 1,
+  Medium,
+  Large,
+}
+let Mysize: Size = Size.Medium
+console.log(Mysize)
+
+// functions
+// properly annotate parameters and return types
+function calc(tax: number): number {
+  if (tax < 2023) return 10
+  return 15
+}
+
+// objects
+
+let employee: {
+  id: number
+  name: string
+  retire: (date: Date) => void
+} = {
+  id: 1,
+  name: "userName",
+  retire(date: Date) {
+    console.log(date)
+  },
+}
