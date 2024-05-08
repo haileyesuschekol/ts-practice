52053e12edbba0313c72ef92f988cfd73897b0dd
Haileyesus Chekol <teamdevs7@gmail.com>
2024-05-08 11:12:34 +0100

    generic function

--- src/generic.ts
+++ src/generic.ts
@@ -1,4 +1,12 @@
+// Generic class
 class KeyPair<k, v> {
   constructor(public key: k, public value: v) {}
 }
 const pair = new KeyPair<string, number>("a", 4)
+
+// Generic function
+function arrayList<v>(value: v) {
+  return [value]
+}
+
+let num = arrayList(1)
